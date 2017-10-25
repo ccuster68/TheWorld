2e787a3001fa5f3ea16da50066c3c685da2f56ff
Charles Custer <charles.custer@kaseya.com>
2017-10-24 17:08:10 -0700

    Finished Bootstrap

--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -7,22 +7,63 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace TheWorld
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("config.json")
+                .SetBasePath(_env.ContentRootPath)
+                .AddEnvironmentVariables();
+
+            _config = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_config);
+
+            if (_env.IsDevelopment())
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+            else
+            {
+                // Implement Real Mail Service
+            }
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseDefaultFiles();
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" });
+            });
         }
     }
 }
--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -1,15 +1,38 @@
 ﻿//site.js
 (function () {
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Chas";
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function () {
-        main.style.backgroundColor = "#888";
-    };
+    //var ele = $("#username");
+    //ele.text("Chas");
 
-    main.onmouseleave = function () {
-        main.style.backgroundColor = "";
-    };
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color: #888";
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
+
+    //var menuItems = $("ul.menu li a")
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fa")
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
+        } else {
+            $icon.removeClass("fa-angle-right");
+            $icon.addClass("fa-angle-left");
+        }
+    });
+    
+
 })();
 
